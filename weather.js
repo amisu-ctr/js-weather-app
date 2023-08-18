988e0e1fd732f9f10b7c9ff33679e68f6bd6e27d
amisu-ctr <hamisurahman@gmail.com>
2023-08-18 18:44:54 +0000

    fetch api with axios

---
+++ weather.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+// https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime
+
+export function getWeather(lat, lon, timezone) {
+  return axios.get(
+    'https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation_probability,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime',
+    {
+      params: {
+        latitude: lat,
+        longitude: lon,
+        timezone,
+      },
+    }
+  );
+  // .then(({ data }) => {
+  //   return {
+  //     current: parseCurrentWeather(data),
+  //     daily: parseDailyWeather(data),
+  //     hourly: parseHourlyWeather(data),
+  //   };
+  // });
+}
+
+// function parseCurrentWeather({current_weather, daily})
+
+//axios - makes fetching easier
