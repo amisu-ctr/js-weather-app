07a2184be4b65ba9094c8f09b90b9a0abbd791e9
amisu-ctr <hamisurahman@gmail.com>
2023-08-26 00:15:12 +0000

    render api data

--- main.js
+++ main.js
@@ -8,4 +8,26 @@ getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone)
     alert('Error getting weather');
   });
 
-function renderWeather() {}
+function renderWeather({ current, daily, hourly }) {
+  console.log(hourly, daily, current);
+  renderCurrentWeather(current);
+  // renderDailyWeather(daily)
+  // renderHourlyWeather(hourly)
+  document.body.classList.remove('blurred');
+}
+
+const currentIcon = document.querySelector('[data-current-icon]');
+
+function setValue(selector, value, { parent = document } = {}) {
+  parent.querySelector(`[data-${selector}]`).textContent = value;
+}
+
+function renderCurrentWeather(current) {
+  setValue('current-temp', current.currentTemp);
+  setValue('current-high', current.highTemp);
+  setValue('current-low', current.lowTemp);
+  setValue('current-fl-high', current.highFeelsLike);
+  setValue('current-fl-low', current.lowFeelsLike);
+  setValue('current-wind', current.windSpeed);
+  setValue('current-precip', current.precip);
+}
